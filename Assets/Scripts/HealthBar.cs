8ab626e2de4e851342914d2b44d4c3b905c44c44
andrey231102 <73721039+andrey231102@users.noreply.github.com>
2021-08-02 23:33:31 +0300

    Update HealthBar.cs

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -6,13 +6,8 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Slider))]
 public class HealthBar : MonoBehaviour
 {
-<<<<<<< HEAD
     [SerializeField] private float _smoothness;
     [SerializeField] private Player _player;
-=======
-    [SerializeField] private float _maxHealth;
-    [SerializeField] private float _smoothness;
->>>>>>> edf1f816012e06454e78e7e41aa73e75e22dd2a4
 
     private Slider _slider;
 
@@ -29,31 +24,8 @@ public class HealthBar : MonoBehaviour
         _slider.value = health;
     }
 
-<<<<<<< HEAD
     public void SetHealth(float health)
     {
         _slider.value = health;
-=======
-    private void Update()
-    {
-        if (_slider.value != _currentHealth)
-        {
-            _slider.value = Mathf.Lerp(_slider.value, _currentHealth, _smoothness * Time.deltaTime);
-        }
-    }
-
-    public void ChangeHealth(float health)
-    {
-        _currentHealth = health;
-        
-        if (health<0)
-        {
-            _currentHealth = 0;
-        }
-        else if (health>_slider.maxValue)
-        {
-            _currentHealth = _slider.maxValue;
-        }
->>>>>>> edf1f816012e06454e78e7e41aa73e75e22dd2a4
     }
 }
