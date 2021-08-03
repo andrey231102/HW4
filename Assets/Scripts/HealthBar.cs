5790c0dd68dc8f938c3ea5df848ef4e252f8a00d
andrey231102 <73721039+andrey231102@users.noreply.github.com>
2021-08-03 13:29:28 +0300

    Attempt 3

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,54 +6,31 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Slider))]
 public class HealthBar : MonoBehaviour
 {
-<<<<<<< HEAD
     [SerializeField] private float _smoothness;
     [SerializeField] private Player _player;
-=======
-    [SerializeField] private float _maxHealth;
-    [SerializeField] private float _smoothness;
->>>>>>> edf1f816012e06454e78e7e41aa73e75e22dd2a4
 
     private Slider _slider;
 
-    private void Update()
+    private void Start()
     {
-        if (_slider.value != _player.CurrentHealth)
-            _slider.value = Mathf.Lerp(_slider.value, _player.CurrentHealth, _smoothness * Time.deltaTime);
+        SetMaxHealth(_player.MaxHealth);
     }
 
-    public void SetMaxHealth(float health)
+    private void Update()
+    {
+        SetHealth(_player.CurrentHealth);
+    }
+
+    private void SetMaxHealth(float maxHealth)
     {
         _slider = GetComponent<Slider>();
-        _slider.maxValue = health;
-        _slider.value = health;
+        _slider.maxValue = maxHealth;
+        _slider.value = maxHealth;
     }
 
-<<<<<<< HEAD
-    public void SetHealth(float health)
+    private void SetHealth(float currentHealth)
     {
-        _slider.value = health;
-=======
-    private void Update()
-    {
-        if (_slider.value != _currentHealth)
-        {
-            _slider.value = Mathf.Lerp(_slider.value, _currentHealth, _smoothness * Time.deltaTime);
-        }
-    }
-
-    public void ChangeHealth(float health)
-    {
-        _currentHealth = health;
-        
-        if (health<0)
-        {
-            _currentHealth = 0;
-        }
-        else if (health>_slider.maxValue)
-        {
-            _currentHealth = _slider.maxValue;
-        }
->>>>>>> edf1f816012e06454e78e7e41aa73e75e22dd2a4
+        if (_slider.value != currentHealth)        
+            _slider.value = Mathf.MoveTowards(_slider.value, currentHealth, _smoothness * Time.deltaTime);       
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,21 +5,19 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {    
     [SerializeField] private float _maxHealth;
-    [SerializeField] private HealthBar _healthBar;
 
     private float _currentHealth;
 
     public float CurrentHealth => _currentHealth;
+    public float MaxHealth => _maxHealth;
 
     private void Start()
     {
         _currentHealth = _maxHealth;
-        _healthBar.SetMaxHealth(_maxHealth);
     }
 
     public void ChangeHealth(float health)
     {
-        _healthBar.SetHealth(_currentHealth);
         _currentHealth = health;
 
         if (health < 0)
